f65c17c7db1007a35c1bf9edd59b79c1291c7a21
Azam007112 <azamhridoy9@gmail.com>
2017-07-10 11:30:49 +0600

    add new line

--- GitDemoApp/GitDemoApp/Program.cs
+++ GitDemoApp/GitDemoApp/Program.cs
@@ -11,6 +11,7 @@ namespace GitDemoApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World...");
+            Console.WriteLine("Add new line");
         }
     }
 }
